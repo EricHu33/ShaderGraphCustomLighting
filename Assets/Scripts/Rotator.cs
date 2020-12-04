f7ce993148db867d67f2db8e51cbd0ddad3aff81
EricHu33 <eric.hu@aktsk.com>
2020-12-04 18:23:42 +0800

    first commit

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    private int m_angle = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_angle++;
+        if (m_angle >= 360)
+        {
+            m_angle = 0;
+        }
+        transform.RotateAround(transform.position, Vector3.up, 1);
+    }
+}
